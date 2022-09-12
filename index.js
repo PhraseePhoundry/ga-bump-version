adf7e6121f575bec85065398e41b8dbdd20c7175
Emily Birch <emily.birch@phrasee.co>
2022-09-12 12:07:44 +0100

    Added setting custom version number

--- index.js
+++ index.js
@@ -55,30 +55,28 @@ const pkg = getPackageJson();
   }
 
   // input wordings for MAJOR, MINOR, PATCH, PRE-RELEASE
+  const customWords = process.env['INPUT_SET-CUSTOM-VERSION-WORDING'].split(',');
   const majorWords = process.env['INPUT_MAJOR-WORDING'].split(',');
   const minorWords = process.env['INPUT_MINOR-WORDING'].split(',');
   // patch is by default empty, and '' would always be true in the includes(''), thats why we handle it separately
   const patchWords = process.env['INPUT_PATCH-WORDING'] ? process.env['INPUT_PATCH-WORDING'].split(',') : null;
-  const preReleaseWords = process.env['INPUT_RC-WORDING'] ? process.env['INPUT_RC-WORDING'].split(',') : null;
 
-  console.log('config words:', { majorWords, minorWords, patchWords, preReleaseWords });
+  console.log('config words:', { setVersionWords, majorWords, minorWords, patchWords });
 
-  // get default version bump
-  let version = process.env.INPUT_DEFAULT;
-  let foundWord = null;
-  // get the pre-release prefix specified in action
-  let preid = process.env.INPUT_PREID;
+  let version;
+  let customVersionNumber;
 
   // case if version-type found
   if (versionType) {
     version = versionType;
   }
+  // case: if wording for SET CUSTOM VERSION found
+  else if (messages.some((message) => customWords.some((word) => message.includes(word)))) {
+    version = 'custom';
+    customVersionNumber = messages.match(/SET VERSION NUMBER {([^{\}]*)}/)[0]
+  }
   // case: if wording for MAJOR found
-  else if (
-    messages.some(
-      (message) => /^([a-zA-Z]+)(\(.+\))?(\!)\:/.test(message) || majorWords.some((word) => message.includes(word)),
-    )
-  ) {
+  else if (messages.some((message) => majorWords.some((word) => message.includes(word)))) {
     version = 'major';
   }
   // case: if wording for MINOR found
@@ -89,58 +87,12 @@ const pkg = getPackageJson();
   else if (patchWords && messages.some((message) => patchWords.some((word) => message.includes(word)))) {
     version = 'patch';
   }
-  // case: if wording for PRE-RELEASE found
-  else if (
-    preReleaseWords &&
-    messages.some((message) =>
-      preReleaseWords.some((word) => {
-        if (message.includes(word)) {
-          foundWord = word;
-          return true;
-        } else {
-          return false;
-        }
-      }),
-    )
-  ) {
-    if (foundWord !== ''){
-      preid = foundWord.split('-')[1];
-    }
-    version = 'prerelease';
-  }
 
-  console.log('version action after first waterfall:', version);
-
-  // case: if default=prerelease,
-  // rc-wording is also set
-  // and does not include any of rc-wording
-  // then unset it and do not run
-  if (
-    version === 'prerelease' &&
-    preReleaseWords &&
-    !messages.some((message) => preReleaseWords.some((word) => message.includes(word)))
-  ) {
-    version = null;
-  }
-
-  // case: if default=prerelease, but rc-wording is NOT set
-  if (version === 'prerelease' && preid) {
-    version = 'prerelease';
-    version = `${version} --preid=${preid}`;
-  }
-
-  console.log('version action after final decision:', version);
+  console.log('version action:', version);
 
   // case: if nothing of the above matches
   if (!version) {
     exitSuccess('No version keywords found, skipping bump.');
-    return;
-  }
-
-  // case: if user sets push to false, to skip pushing new tag/package.json
-  const push = process.env['INPUT_PUSH'];
-  if (push === 'false' || push === false) {
-    exitSuccess('User requested to skip pushing new tag and package.json. Finished.');
     return;
   }
 
@@ -179,7 +131,18 @@ const pkg = getPackageJson();
     // important for further usage of the package.json version
     await runInWorkspace('npm', ['version', '--allow-same-version=true', '--git-tag-version=false', current]);
     console.log('current 1:', current, '/', 'version:', version);
-    let newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim().replace(/^v/, '');
+
+    let newVersion;
+    if(customVersionNumber) {
+      const versionNumberRegex = new RegExp('^v?[0-9]+[.][0-9]+[.][0-9]+$');
+      if(!versionNumberRegex.test(customVersionNumber)) {
+        exitFailure('Invalid custom version number provided')
+      } else {
+        newVersion = customVersionNumber.replace(/^v/, '');
+      }
+    } else {
+      newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim().replace(/^v/, '');
+    }
     console.log('newVersion 1:', newVersion);
     newVersion = `${tagPrefix}${newVersion}`;
     if (process.env['INPUT_SKIP-COMMIT'] !== 'true') {
@@ -211,7 +174,7 @@ const pkg = getPackageJson();
     } catch (e) {
       console.warn(
         'git commit failed because you are using "actions/checkout@v2"; ' +
-          'but that doesnt matter because you dont need that git commit, thats only for "actions/checkout@v1"',
+          'but that doesnt matter because you dont need that git commit, that\'s only for "actions/checkout@v1"',
       );
     }
 
